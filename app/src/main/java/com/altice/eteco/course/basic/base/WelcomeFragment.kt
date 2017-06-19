07ab757f65fb37559ca55db01f814f7b415e385d
Fausto Bencosme <fbencosme@kitsd.com>
2017-06-19 02:41:37 -0400

    Welcome fragment animation

--- app/src/main/java/com/altice/eteco/course/basic/base/WelcomeFragment.kt
+++ app/src/main/java/com/altice/eteco/course/basic/base/WelcomeFragment.kt
@@ -1,10 +1,24 @@
 package com.altice.eteco.course.basic.base
 
+import android.os.Bundle
+import android.view.View
 import com.altice.eteco.course.basic.BaseFragment
 import com.altice.eteco.course.basic.R
+import com.pawegio.kandroid.loadAnimation
+import kotlinx.android.synthetic.main.welcome_fragment.*
 
 class WelcomeFragment : BaseFragment() {
 
     override val layoutRes: Int = R.layout.welcome_fragment
-    override val titleRes: Int = R.string.welcome_title
+    override val titleRes : Int = R.string.welcome_title
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(this.context.loadAnimation(R.anim.abc_slide_in_bottom)) {
+            duration = 1000
+            welcome.startAnimation(this)
+        }
+
+    }
 }
