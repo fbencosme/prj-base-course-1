ae7071651e58720bb4b879ea03195be3c37bfcf0
Fausto Bencosme <fbencosme@kitsd.com>
2017-07-01 10:35:43 -0400

    TicTacToe improvements

--- app/src/main/java/com/altice/eteco/course/basic/ticTacToe/TicTacToeFragment.kt
+++ app/src/main/java/com/altice/eteco/course/basic/ticTacToe/TicTacToeFragment.kt
@@ -1,20 +1,29 @@
 package com.altice.eteco.course.basic.ticTacToe
 
-import android.graphics.drawable.ColorDrawable
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
-import android.support.v7.widget.AppCompatTextView
-import android.view.LayoutInflater
+import android.support.v4.content.ContextCompat
 import android.view.View
-import android.view.ViewGroup
 import android.widget.TextView
 
 import com.altice.eteco.course.basic.BaseFragment
 import com.altice.eteco.course.basic.R
 import com.jakewharton.rxbinding2.view.clicks
+import com.pawegio.kandroid.alert
+import com.pawegio.kandroid.longToast
+import com.trello.rxlifecycle2.android.FragmentEvent
+import io.reactivex.Observable
 import io.reactivex.Observable.merge
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.rxkotlin.Observables
 import io.reactivex.rxkotlin.merge
+import io.reactivex.subjects.PublishSubject
+import  io.reactivex.rxkotlin.withLatestFrom
 
 import kotlinx.android.synthetic.main.tic_tac_toe_fragment.*
+import java.util.concurrent.TimeUnit
 
 
 class TicTacToeFragment : BaseFragment() {
@@ -22,20 +31,108 @@ class TicTacToeFragment : BaseFragment() {
     override val layoutRes: Int = R.layout.tic_tac_toe_fragment
     override val titleRes : Int = R.string.ticTacToe_title
 
-    var gridBtns : Iterable<TextView> = listOf()
+    var gridBtns = listOf<Pair<Int, TextView>>()
+    val moves    = PublishSubject.create<List<Move>>()
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        gridBtns = (0 .. grid.childCount - 1).map {  grid.getChildAt(it) as TextView }
-        val bg   = windowBackgroundColor()
-        gridBtns.forEach { it.setBackgroundColor(bg) }
-
-        val clicks = gridBtns .map { tv -> tv.clicks().map { tv } }.merge()
-         .subscribe {
-            it.text = if (it.id % 2 == 0) "O" else "X"
+        gridBtns = (0 .. grid.childCount - 1).map {
+            Pair(it, grid.getChildAt(it) as TextView)
         }
 
+        // Configure button background.
+        val bg   = windowBackgroundColor()
+        gridBtns.forEach {
+            (_, tv) -> tv.setBackgroundColor(bg)
+        }
+
+        // Reset Game.
+        reset
+            .clicks()
+            .compose(bindUntilEvent(FragmentEvent.DESTROY_VIEW))
+            .subscribe { doReset() }
+
+        // Game stream flow.
+        gridBtns.map {
+            (position, tv) ->
+                tv.clicks().map { Pair(position, tv) }
+        }
+        .merge()
+        .throttleFirst(600, TimeUnit.MILLISECONDS)
+        .withLatestFrom(moves.startWith(emptyList<Move>())) {
+             (position, tv), moves -> Triple(position, tv, moves)
+         }
+        .filter {
+            (position, tv, moves) -> !moves.any { it.position == position }
+        }
+        .map(TicTacToe::nextMove)
+        .doOnNext {
+            (ms, m, _) ->
+                moves.onNext(ms + m)
+        }
+        .observeOn(AndroidSchedulers.mainThread())
+        .compose(bindUntilEvent(FragmentEvent.DESTROY_VIEW))
+        .subscribe {
+            (_, m, tv) ->
+
+              when(m.symbol) {
+                  Symbol.Cross  -> tv.text = "X"
+                  Symbol.Nought -> tv.text = "O"
+              }
+        }
+
+        // Check Game winner
+        moves
+            .filter { it.any() }
+            .map(TicTacToe::checkMove)
+            .compose(bindUntilEvent(FragmentEvent.DESTROY_VIEW))
+            .subscribe {
+                (s, poss, moves) ->
+                when(s) {
+                    MoveState.Next -> {}
+
+                    MoveState.Tie  -> {
+                        context.alert {
+                            title("Tie")
+                            onCancel { doReset()  }
+                        }
+                     //   doReset()
+                    }
+
+                    MoveState.Winner  -> {
+                        context.alert("Win").show()
+                        markWinner(poss, moves.last())
+                    }
+                }
+            }
+    }
+
+    fun markWinner(poss: Array<Int>, move: Move) {
+        val colorRes = if (move.symbol == Symbol.Nought) R.color.colorAccent else R.color.colorPrimary
+        var color    = ContextCompat.getColor(context, colorRes)
+
+        gridBtns
+            .filter  { (p, _ )  -> poss.contains(p) }
+            .forEach { (_, tv) ->
+                tv.setTextColor(color)
+                tv.setTypeface(tv.typeface, Typeface.BOLD_ITALIC)
+                //tv.alpha = .5f
+            }
+    }
+
+    fun doReset() {
+        val c = ContextCompat.getColor(context, android.R.color.black)
+
+        moves.onNext(emptyList())
+
+        gridBtns.forEach {
+            (_, tv) ->
+                tv.text = ""
+                tv.setTypeface(tv.typeface, Typeface.NORMAL)
+                //tv.alpha = 1f
+                tv.setTextColor(c)
+        }
     }
 
 }
--- app/src/main/java/com/altice/eteco/course/basic/ticTacToe/data.kt
+++ app/src/main/java/com/altice/eteco/course/basic/ticTacToe/data.kt
@@ -1,9 +1,69 @@
 package com.altice.eteco.course.basic.ticTacToe
 
-enum class Type {
+import android.widget.TextView
+
+enum class Symbol {
     Cross, Nought
 }
 
-data class TicTacToe(
-    val type : Type
+data class Move(
+    val symbol  : Symbol,
+    val position: Int
 )
+
+enum class MoveState {
+    Next, Winner, Tie
+}
+
+object TicTacToe {
+
+    fun checkMove(moves: List<Move>) : Triple<MoveState, Array<Int>, List<Move>> {
+
+        val positions = moves.map { it.position }
+
+        fun checkMatch (match: Array<Int>) : Boolean {
+            val ms = moves.filter { m -> match.contains(m.position) }
+            return ms.all { it.symbol == Symbol.Nought } ||
+                   ms.all { it.symbol == Symbol.Cross  }
+        }
+
+        val winner = wins.find {
+            it.all { positions.contains(it) } && checkMatch(it)
+        }
+
+        if (winner != null)
+            return Triple(MoveState.Winner, winner, moves)
+
+        if (moves.size == 9)
+            return Triple(MoveState.Tie, emptyArray(), moves)
+
+        return Triple(MoveState.Next, emptyArray(), moves)
+    }
+
+    fun nextMove(t: Triple<Int, TextView, List<Move>>) : Triple<List<Move>, Move, TextView> {
+        val (position, tv, moves) = t
+        val last = moves.lastOrNull()
+        if (last == null)
+            return Triple(moves, Move(Symbol.Cross, position), tv)
+        else
+            return Triple(moves, Move(last.symbol.opposite(), position), tv)
+    }
+
+    val wins = arrayOf(
+        arrayOf(0, 1, 2),
+        arrayOf(3, 4, 5),
+        arrayOf(6, 7, 8),
+
+        arrayOf(0, 3, 6),
+        arrayOf(1, 4, 7),
+        arrayOf(2, 5, 8),
+
+        arrayOf(0, 4, 8),
+        arrayOf(2, 4, 6))
+}
+
+fun Symbol.opposite() =
+    when(this) {
+        Symbol.Cross  -> Symbol.Nought
+        Symbol.Nought -> Symbol.Cross
+    }
