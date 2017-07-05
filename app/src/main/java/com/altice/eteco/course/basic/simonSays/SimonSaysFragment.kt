8410387be57c4bf6e25eb318b48c1e46771fab7a
Fausto Bencosme <fbencosme@kitsd.com>
2017-07-05 13:35:54 -0400

    Adding some resources

--- app/src/main/java/com/altice/eteco/course/basic/simonSays/SimonSaysFragment.kt
+++ app/src/main/java/com/altice/eteco/course/basic/simonSays/SimonSaysFragment.kt
@@ -1,5 +1,8 @@
 package com.altice.eteco.course.basic.simonSays
 
+import android.media.MediaPlayer
+import android.os.Bundle
+import android.view.View
 import com.altice.eteco.course.basic.BaseFragment
 import com.altice.eteco.course.basic.R
 
@@ -7,4 +10,14 @@ class SimonSaysFragment : BaseFragment() {
 
     override val layoutRes: Int = R.layout.simon_says_fragment
     override val titleRes : Int = R.string.simonSays_title
+
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        R.raw.sound1
+    }
+
+    fun Item.play() {
+        MediaPlayer.create(context, sound).start()
+    }
 }
---
+++ app/src/main/java/com/altice/eteco/course/basic/simonSays/data.kt
@@ -0,0 +1,6 @@
+package com.altice.eteco.course.basic.simonSays
+
+data class Item(
+    val sound: Int,
+    val color: Int
+)
