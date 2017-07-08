1522b7b68d811eaae8d200c30ed3633ce4fbdf88
Fausto Bencosme <fbencosme@kitsd.com>
2017-07-08 08:52:38 -0400

    Merging

--- app/src/main/java/com/altice/eteco/course/basic/connect4/Connect4Fragment.kt
+++ app/src/main/java/com/altice/eteco/course/basic/connect4/Connect4Fragment.kt
@@ -2,20 +2,25 @@ package com.altice.eteco.course.basic.connect4
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 
 import com.altice.eteco.course.basic.BaseFragment
 import com.altice.eteco.course.basic.R
+import com.altice.eteco.course.basic.simonSays.play
 
 import com.jakewharton.rxbinding2.view.clicks
 
 import com.pawegio.kandroid.views
+import com.trello.rxlifecycle2.android.FragmentEvent
 
 import io.reactivex.rxkotlin.merge
 import io.reactivex.rxkotlin.withLatestFrom
 import io.reactivex.subjects.PublishSubject
+import io.reactivex.subjects.BehaviorSubject
+import io.reactivex.Observable.merge
+import io.reactivex.android.schedulers.AndroidSchedulers
 
 import kotlinx.android.synthetic.main.connect_four_fragment.*
+import java.util.concurrent.TimeUnit
 
 class Connect4Fragment : BaseFragment() {
 
@@ -23,43 +28,119 @@ class Connect4Fragment : BaseFragment() {
     override val titleRes : Int = R.string.conn4_title
 
     val board = PublishSubject.create<List<Bucket>>()
-    val turn  = PublishSubject.create<Turn>()
+    val turn  = BehaviorSubject.createDefault(Turn.Red)
+    val wins  = PublishSubject.create<Turn>()
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val view = grid.views
+        val views = grid.views
 
-        val btns = grid.views.map {
+        val chips = grid.views.map {
+
             val tag = it.tag.toString()
+            val x   = "${tag.first()}".toInt()
+            val y   = "${tag.last()}".toInt()
 
-            Chip(view.indexOf(it), it, tag.first().toInt(), tag.last().toInt())
-        }
-
-        btns.map { c ->
-            c.view.clicks().map { c }
-        }
-        .merge()
-        .withLatestFrom(board.startWith(emptyList<Bucket>())) {
-            c, board -> Pair(c, board)
-        }
-        .filter {
-            (c, board) -> board.none { it.x == c.x && it.y == c.y}
-        }
-        .withLatestFrom(turn.startWith(Turn.Red).doOnNext {
-            onTurn.background = it.drawable(context)
-        }) {
-            (c, b), t -> Triple(c, t, b)
-        }
-        .doOnNext  {
-            (c, t, b) ->
-              board.onNext(b + Bucket(c.x, c.y))
-        }
-        .subscribe { (c, t, b) ->
-
-            c.view.background = t.drawable(context)
-            turn.onNext(t.flip())
+            Chip(views.indexOf(it), it, x, y)
         }
 
+        val winner = wins.map { w ->
+            { ->
+                views.forEach {
+                    it.isEnabled  = false
+                    it.alpha      = .4f
+                }
+                when (w)  {
+
+                    Turn.Red    -> {
+                        red.bump()
+                        R.raw.applause.play(context) {
+                            doReset(views)
+                        }
+                    }
+
+                    Turn.Yellow -> {
+                        yellow.bump()
+                        R.raw.applause.play(context) {
+                            doReset(views)
+                        }
+                    }
+
+                    Turn.Black -> doReset(views)
+                }
+        } }
+
+        val restart = reset.clicks().map { _ ->
+            {
+                doReset(views)
+            }
+        }
+
+        val game =
+            chips.map { chip ->
+                chip.view.clicks().map { chip }
+            }
+            .merge()
+            .throttleFirst(300, TimeUnit.MILLISECONDS)
+            .withLatestFrom(board.startWith(emptyList<Bucket>())) {
+                chip, board -> Pair(chip, board)
+            }.withLatestFrom(
+                turn.doOnNext { onTurn.background = it.drawable(context) }
+            ) {
+                (chip, board), t -> Triple(chip, board, t)
+            }
+            .map {
+                (chip, board, turn) ->
+                    val bucket  = Game.findBucket(turn, chip, board)
+                    val newChip = chips.find { it.x == bucket.x && it.y == bucket.y }
+                    Triple(Pair(newChip, bucket), board, turn)
+            }
+            .filter {
+                (pair, board, _) ->
+                    val chip = pair.first
+                    board.isEmpty() || board.none { it.x == chip?.x && it.y == chip.y }
+            }
+            .doOnNext {
+                (pair, bucket, _) ->
+                    val newBucket = pair.second
+                    board.onNext(bucket + newBucket)
+            }
+            .map { (pair, b, t) -> {
+                ->
+                    pair.first?.let {
+                        it.view.background = t.drawable(context)
+                    }
+
+                    turn.onNext(t.flip())
+
+                    val tmp = b + pair.second
+
+                    if (Game.check(t, tmp))
+                        wins.onNext(t)
+                    else if (tmp.size == 42)
+                        wins.onNext(Turn.Black)
+                }
+            }
+
+        // Game flow.
+        merge(game, restart, winner)
+            .observeOn(AndroidSchedulers.mainThread())
+            .compose(bindUntilEvent(FragmentEvent.DESTROY_VIEW))
+            .subscribe {
+                it?.invoke()
+            }
     }
+
+    fun doReset(views: List<View>) {
+        val bg = Turn.Black.drawable(context)
+        views.forEach {
+            it.background = bg
+            it.isEnabled  = true
+            it.alpha      = 1f
+        }
+        board.onNext(emptyList())
+        turn.onNext(Turn.Red)
+    }
+
 }
--- app/src/main/java/com/altice/eteco/course/basic/connect4/data.kt
+++ app/src/main/java/com/altice/eteco/course/basic/connect4/data.kt
@@ -4,11 +4,12 @@ import android.content.Context
 import android.graphics.drawable.Drawable
 import android.support.v4.content.ContextCompat
 import android.view.View
+import android.widget.TextView
 
 import com.altice.eteco.course.basic.R
 
 enum class Turn {
-    Red, Yellow
+    Red, Yellow, Black
 }
 
 data class Chip(
@@ -19,13 +20,15 @@ data class Chip(
 )
 
 data class Bucket(
-    val x: Int,
-    val y: Int
+    val x    : Int,
+    val y    : Int,
+    val turn : Turn
 )
 
 fun Turn.flip() : Turn = when(this) {
     Turn.Yellow -> Turn.Red
     Turn.Red    -> Turn.Yellow
+    Turn.Black  -> Turn.Black
 }
 
 fun Turn.drawable(ctx: Context) : Drawable {
@@ -33,6 +36,7 @@ fun Turn.drawable(ctx: Context) : Drawable {
     val res = when (this) {
         Turn.Red    -> R.drawable.circle_red
         Turn.Yellow -> R.drawable.circle_yellow
+        Turn.Black  -> R.drawable.circle_black
     }
 
     return ContextCompat.getDrawable(ctx, res)
@@ -40,12 +44,96 @@ fun Turn.drawable(ctx: Context) : Drawable {
 
 object Game {
 
-    fun findBucket(c: Chip, board: List<Bucket>) : Bucket {
-        val b = board.firstOrNull { it.y == c.y }
+    fun findBucket(t: Turn, c: Chip, board: List<Bucket>) : Bucket {
+        val ys = board.filter { it.y == c.y  }
+        val b  = ys.sortedBy { it.x }.lastOrNull()
+
         if (b == null)
-            return Bucket(c.x, 0)
+            return Bucket(5, c.y, t)
         else
-            return Bucket(c.x, b.y + 1)
+            return Bucket(5 - ys.size, c.y, t)
 
     }
+
+    fun check(t: Turn, board: List<Bucket>) : Boolean {
+
+        var win = checkVH(t, board, vertical  , { it.y }, { it.x }) ||
+                checkVH(t, board, horizontal, { it.x }, { it.y }) ||
+                checkDA(t, board) ||
+                checkDD(t, board)
+
+        return win
+    }
+
+    fun checkVH(t: Turn, board: List<Bucket>, orientation: List<IntRange>, group: (Bucket) -> Int, map : (Bucket) -> Int) : Boolean =
+        board
+            .groupBy { group(it) }
+            .filter { it.value.size >= 4 }
+            .any {
+
+            val xs = it.value.map { map(it) }.sorted()
+
+            val match = orientation.any { w ->
+                xs.all { x -> w.contains(x) }
+            }
+            val match2 = it.value.count { it.turn == t } >= 4
+            match && match2
+        }
+
+    fun checkDA(t: Turn, board: List<Bucket>) :  Boolean {
+        var res = false
+
+        for (x in 3..6) {
+            for (y in 0..2) {
+
+                val a = board.find { it.x == x   && it.y == y   }
+                val b = board.find { it.x == x-1 && it.y == y+1 }
+                val c = board.find { it.x == x-2 && it.y == y+2 }
+                val d = board.find { it.x == x-3 && it.y == y+3 }
+
+                if (a?.turn?.equals(t) ?: false &&
+                    b?.turn?.equals(t) ?: false &&
+                    c?.turn?.equals(t) ?: false &&
+                    d?.turn?.equals(t) ?: false)
+                {
+                    res = true
+                    return res
+                }
+            }
+        }
+
+        return res
+    }
+
+    fun checkDD(t: Turn, board: List<Bucket>) :  Boolean {
+        var res = false
+
+        for (x in 3..6) {
+            for (y in 0..5) {
+
+                var a = board.find { it.x == x   && it.y == y   }
+                var b = board.find { it.x == x-1 && it.y == y-1 }
+                var c = board.find { it.x == x-2 && it.y == y-2 }
+                var d = board.find { it.x == x-3 && it.y == y-3 }
+
+                if (a?.turn?.equals(t) ?: false &&
+                    b?.turn?.equals(t) ?: false &&
+                    c?.turn?.equals(t) ?: false &&
+                    d?.turn?.equals(t) ?: false)
+                {
+                    res = true
+                    return res
+                }
+            }
+        }
+
+        return res
+    }
+
+    val horizontal = listOf(0..3, 1..4, 2..5, 3..6)
+    val vertical   = listOf(0..3, 1..4, 2..5)
+}
+
+fun TextView.bump(n: Int = 1) {
+    this.text = ("${this.text}".toInt() + n).toString()
 }
