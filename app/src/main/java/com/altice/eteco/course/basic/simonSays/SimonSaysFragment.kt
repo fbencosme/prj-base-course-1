9b7d592d6b63e1d027a7f72373632a9df9160d61
Fausto Bencosme <fbencosme@kitsd.com>
2017-07-09 14:50:27 -0400

    Simon refactor

--- app/src/main/java/com/altice/eteco/course/basic/simonSays/SimonSaysFragment.kt
+++ app/src/main/java/com/altice/eteco/course/basic/simonSays/SimonSaysFragment.kt
@@ -42,20 +42,66 @@ class SimonSaysFragment : BaseFragment() {
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val game = Game(listOf(green, red, yellow, blue))
-        val bump = PublishSubject.create<Boolean>()
+        val options = Game(listOf(green, red, yellow, blue))
+        val bump    = PublishSubject.create<Boolean>()
 
-        val counter = seq.map { it.size }.map { size ->
+        // Display wait message stream.
+        val autoMsg = waitStream()
+
+        // Score
+        val counter = counterStream()
+
+        // Wins!
+        val winner = winnerStream(bump, options)
+
+        // Start action button.
+        val start = startStream(options)
+
+        // On click on item/Button stream.
+        val play = playStream(options, bump)
+
+        // Auto play stream.
+        var auto = autoPlayStream()
+
+        // Game
+        val game = merge(start, winner, auto, play)
+            .mergeWith(counter)
+            .mergeWith(autoMsg)
+
+        // Game flow.
+        game
+            .observeOn(AndroidSchedulers.mainThread())
+            .compose(bindUntilEvent(FragmentEvent.DESTROY_VIEW))
+            .subscribe(
+                { it -> it.invoke() },
+                { it  -> print(it.message) },
+                { println("completed") }
+            )
+    }
+
+    fun waitStream(): Observable<() -> Unit>? =
+         autoPlay.map { auto ->  {
+             if (auto)
+                autoOn.visibility = View.VISIBLE
+             else {
+                 toast(R.string.simon_says_go)
+                 autoOn.visibility = View.INVISIBLE
+             }
+        }
+    }
+
+    fun counterStream(): Observable<() -> Unit>? =
+        seq.map { it.size }.map { size ->
             { ->
                 val c = if (size <= 0) 0 else size - 1
                 score.text = "$c"
             }
         }
 
-        // Wins!
-        val winner = bump.delay(200, TimeUnit.MILLISECONDS)
-            .withLatestFrom(seq) {
-                b, s -> Pair(b, s)
+    fun winnerStream(bump: PublishSubject<Boolean>, game: Game): Observable<() -> Unit>? =
+        bump.delay(200, TimeUnit.MILLISECONDS)
+            .withLatestFrom(seq) { b, s ->
+                    Pair(b, s)
             }
             .map { (bump, s) -> {
 
@@ -72,70 +118,68 @@ class SimonSaysFragment : BaseFragment() {
             }
         }
 
-        // Start action button.
-        val start = go.clicks()
-            .doOnNext {
-                go.hide()
-                score.show()
-                scoreLbl.show()
+    fun startStream(game: Game): Observable<() -> Unit>? =
+        go.clicks()
+          .doOnNext {
+            go.hide()
+            score.show()
+            scoreLbl.show()
+          }
+          .throttleFirst(300, TimeUnit.MILLISECONDS)
+          .map { _ -> { ->
+                longToast(R.string.simon_says_focus)
+                seq.onNext(game.genSeq())
+                autoPlay.onNext(true)
             }
-            .throttleFirst(300, TimeUnit.MILLISECONDS)
-            .map { _ -> { ->
-                    longToast(R.string.simon_says_focus)
-                    seq.onNext(game.genSeq())
-                    autoPlay.onNext(true)
-                }
-            }
+        }
 
-        // On click on item/Button stream.
-        val play = merge(
+    fun playStream(game: Game, bump: PublishSubject<Boolean>): Observable<() -> Unit>? =
+        merge(
             green .clicks().map { game.items[0] },
             red   .clicks().map { game.items[1] },
             yellow.clicks().map { game.items[2] },
-            blue  .clicks().map { game.items[3] })
-        .withLatestFrom(autoPlay) {
-            item, autoPlay -> Pair(item, autoPlay)
+            blue  .clicks().map { game.items[3] }
+         )
+         .withLatestFrom(autoPlay) { item, autoPlay ->
+            Pair(item, autoPlay)
+         }
+         .filter { (_, autoPlay) -> !autoPlay }
+         .map    { it.first }
+         .withLatestFrom(seq.map { it.copy() }) { item, seq ->
+            Pair(item, seq)
+         }
+        .map { (item, seq) ->
+            val poll  = seq.poll()
+            val match = item.button.equals(poll?.button)
+            Triple(match, seq, item)
         }
-        .filter { (_, autoPlay) ->
-            !autoPlay
-        }
-        .map { it.first }
-        .withLatestFrom(seq.map { it.copy() }) {
-            item, seq -> Pair(item, seq)
-        }
-        .map {
-            (item, seq) ->
-                val poll  = seq.poll()
-                val match = item.button.equals(poll?.button)
-                Triple(match, seq, item)
-        }
-        .map {
-            (match, seq, item) -> { ->
+        .map { (match, seq, item) -> { ->
 
-               if (match && seq.isEmpty()) {
+            if (match && seq.isEmpty()) {
 
-                   item.sound.play(context) {
-                       bump.onNext(true)
-                   }
+                item.sound.play(context) {
+                    bump.onNext(true)
+                }
 
-               } else if (match) {
+            } else if (match) {
 
-                   item.sound.play(context)
+                item.sound.play(context)
 
-               } else {
+            } else {
 
-                   toast(R.string.simon_says_aww)
-                   R.raw.aww.play(context) {
-                       bump.onNext(false)
-                   }
-               }
+                toast(R.string.simon_says_aww)
+                R.raw.aww.play(context) {
+                    bump.onNext(false)
+                }
             }
         }
+    }
 
-        // Auto play stream.
-        var auto = autoPlay.filter { it }
+    fun autoPlayStream(): Observable<() -> Unit>? =
+        autoPlay
+            .filter { it }
             .delay(300, TimeUnit.MILLISECONDS)
-            .withLatestFrom(seq) { _ , seq -> seq }
+            .withLatestFrom(seq) { _, seq -> seq }
             .switchMap { seq ->
                 // Emit auto value/sound each 2 seconds.
                 Observable
@@ -145,31 +189,15 @@ class SimonSaysFragment : BaseFragment() {
                         Observable.just(it).delay(3, TimeUnit.SECONDS)
                     }
             }
-            .map {
-                (item, last) -> {
-                    item.button.alpha = .2f
-                    item.sound.play(context) {
+            .map { (item, last) -> {
+                item.button.alpha = .2f
+                item.sound.play(context) {
 
-                        item.button.alpha = 1f
+                    item.button.alpha = 1f
 
-                        if (last)
-                            autoPlay.onNext(false)
-                    }
+                    if (last)
+                        autoPlay.onNext(false)
+                }
             }
         }
-
-        // Game flow.
-        merge(start, winner, auto, play)
-            .mergeWith(counter)
-            .observeOn(AndroidSchedulers.mainThread())
-            .compose(bindUntilEvent(FragmentEvent.DESTROY_VIEW))
-            .subscribe(
-                { it -> it.invoke() },
-                { e  ->
-                    print(e.message)
-                },
-                { println("completed") }
-            )
-
-    }
 }
